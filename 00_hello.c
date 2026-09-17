9c4874a9c829e0331b98123824b6815b8c5a465f
manuuuuuuuuu17 <manushree.1712@gmail.com>
2026-09-17 20:51:13 +0530

    update

--- 00_hello.c
+++ 00_hello.c
@@ -2,7 +2,7 @@
 
 int main(){
     
-    printf("hiiii   \n ");
+    // printf("hiiii   \n ");
 
     // int x;
     // scanf("%d", &x);    // &x is used to write to the variable which holda the int
