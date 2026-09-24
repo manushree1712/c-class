f9375cdf8bc4e91cca752ee59b70788944bb6d60
manuuuuuuuuu17 <manushree.1712@gmail.com>
2026-09-24 10:23:19 +0530

    update

---
+++ 09_problems.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+
+    // float c,f,k;
+    // printf("the temp of c :");
+    // scanf("%f",&c);
+
+    // f=(c*9.0f/5.0f)+32;
+
+    // printf("the temp of f : %f", f);
+    
+
+    // k=c+273.15f;
+     
+    // printf("the temp of k : %f",k);
+    
+
+
+    // 2
+
+    // int x,y,z;
+    // printf("enter num for x: ");  scanf("%d", &x);
+    // printf("enter num for y: ");  scanf("%d", &y);
+    // printf("enter num for z: ");  scanf("%d", &z);
+
+
+    // if (x>y && x>z){
+    //     printf("x is the greatest number");
+    // }
+    // else if (y>x && y>z){
+    //     printf("y is the greatest number");
+    // }
+    // else if (z>x && z>x){
+    //     printf("z is the greatest number");
+    // }
+    // else{
+    //     printf("invalid");
+    // }
+
+}
